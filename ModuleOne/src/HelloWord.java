58785abc7ca475306982bb7c4a9b3cf9bac1e143
Gustavo Solar Gaona <gustavo.solar@elektra.com.mx>
2022-08-17 11:04:10 -0500

    agregando descripcion de variables

---
+++ ModuleOne/src/HelloWord.java
@@ -0,0 +1,25 @@
+public class HelloWord {
+    public static void main(String[] args){
+        /*
+         * Variables, use camelCase in the declaration, example:
+         * firstName and class with HelloWorld
+         */
+        String greeting = "Hello World from Java ...";
+
+        System.out.println(greeting);
+
+        /*
+         * Primitive data types
+         *
+         * boolean -> only true or false, default value is false
+         * char -> character, only one letter or unicode '\u0021'
+         * int -> ..., -1, 0, 1 ...
+         * long -> between -922337203685477808 to 922337203685477807
+         * byte -> between -128 to 127
+         * short -> between -32768 to 32767
+         * float -> between -1.4E to 3.40E
+         * double -> between -4.9E to 1.79E
+         *
+         */
+    }
+}
