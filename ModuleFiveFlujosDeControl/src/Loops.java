d2e4c81d3e8d8f9b6f6be46f9eebe799845ce7d2
Gustavo Solar Gaona <gustavo.solar@elektra.com.mx>
2022-08-31 12:49:28 -0500

    agregando loops while and do-while

--- ModuleFiveFlujosDeControl/src/Loops.java
+++ ModuleFiveFlujosDeControl/src/Loops.java
@@ -1,13 +1,14 @@
 public class Loops {
     public static void main(String[] args) {
+        System.out.println("\nfor");
         // FOR
         // si el bloque de codigo es una sola linea se puede usar sin las llaves
         // de lo contrario se deben usar las llaves
-        for(int iterador = 0; iterador <= 10; iterador++){
+        for(int iterador = 0; iterador <= 5; iterador++){
             System.out.println("ronda 1: " + iterador);
         }
 
-        System.out.println("\n");
+        System.out.println("\nfor desestructurado");
 
         // for desestructurado
         int iteradorDos = 0;
@@ -18,5 +19,30 @@ public class Loops {
             System.out.println("ronda 2: " + iteradorDos);
             iteradorDos++;
         }
+
+        System.out.println("\nforeach");
+
+        String[] namesPets = {"chanchito", "chanchito feliz", "borreguito"};
+        for (String namePet: namesPets) {
+            System.out.println("namePet = " + namePet);
+        }
+
+        System.out.println("\nwhile");
+
+        // while
+        int contador = 0;
+        while(contador < 5){
+            System.out.println("ronda 3: " + contador);
+            contador++;
+        }
+
+        System.out.println("\ndo-while");
+
+        // do-while
+        int contadorNuevo = 0;
+        do{
+            System.out.println("se ejecuta al menos una vez");
+        }while (contadorNuevo != 0);
+
     }
 }
---
+++ ModuleFiveFlujosDeControl/src/sentenciaForArreglos.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class sentenciaForArreglos {
+    public static void main(String[] args) {
+        // definicion de arreglos
+        //String[] names = new String[6]; // se define la longitud que tendra el arreglo
+        String[] pets = {"cat", "dog", "mouse"}; // se definen los valores que tendra el array, como buena practica ponerlo en el tipo
+        //String foods[] = {"tacos", "cemitas", "pizza"}; // se puede definir como arreglo en el nombre
+
+        int count = pets.length; // por buenas practicas se podria declarar el contador fuera del for para solo inicializarlo una vez
+        for (int i=0; i < count; i++){
+            System.out.println("pets number: " + i + " type: " + pets[i]);
+        }
+
+        String pet = JOptionPane.showInputDialog("Enter a pet type");
+        System.out.println("type pet to search = " + pet);
+
+        boolean encontrado = false;
+
+        for (String item : pets) {
+            if (item.equalsIgnoreCase(pet)) {
+                encontrado = true;
+                System.out.println("si se encontro el tipo de mascota");
+                break;
+            }
+        }
+    }
+}
