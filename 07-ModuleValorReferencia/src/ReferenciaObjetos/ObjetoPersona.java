aa1a73015d582eb95e7864df64740de40b12fe28
Gustavo Solar Gaona <gustavo.solar@elektra.com.mx>
2022-09-05 12:03:21 -0500

    agregando valores por referencia en un objeto

---
+++ 07-ModuleValorReferencia/src/ReferenciaObjetos/ObjetoPersona.java
@@ -0,0 +1,18 @@
+package ReferenciaObjetos;
+
+public class ObjetoPersona {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.asignarNuevoNombre("Gustavo");
+        System.out.println("nombre persona " + persona.getName());
+
+        persona.asignarNuevoNombre("Chanchito feliz");
+        System.out.println("nuevo nombre de la persona " + persona.getName());
+    }
+
+
+
+
+}
---
+++ 07-ModuleValorReferencia/src/ReferenciaObjetos/Persona.java
@@ -0,0 +1,13 @@
+package ReferenciaObjetos;
+
+public class Persona {
+    private String name;
+
+    public void asignarNuevoNombre(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
