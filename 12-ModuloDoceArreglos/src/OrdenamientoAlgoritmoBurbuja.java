d45f49ca5793a6757bd77948ad7942eec5e59b1b
Gustavo Solar Gaona <gustavo.solar@elektra.com.mx>
2022-09-07 14:01:31 -0500

    agregando metodo burbuja y una optimizacion a las iteraciones

--- 12-ModuloDoceArreglos/src/OrdenamientoAlgoritmoBurbuja.java
+++ 12-ModuloDoceArreglos/src/OrdenamientoAlgoritmoBurbuja.java
@@ -25,5 +25,28 @@ public class OrdenamientoAlgoritmoBurbuja {
         for (String nombre : nombres) {
             System.out.println("nombre del array Nombres ordenado = " + nombre);
         }
+
+
+        // metodo burbuja optimizado en las iteraciones
+        int vueltas = 0;
+        int sizeArrayNombres = nombres.length;
+
+        for (int i = 0; i < sizeArrayNombres; i++){
+            for (int j = 0; j < sizeArrayNombres-1-i; j++){
+                if(nombres[j+1].compareTo(nombres[j]) < 0){
+                    String aux = nombres[j];
+                    nombres[j] = nombres[j+1];
+                    nombres[j+1] = aux;
+                }
+                vueltas++;
+            }
+        }
+
+        System.out.println("\nnumero de iteraciones optimizadas para ordenar el array = " + vueltas);
+
+        for (String nombre : nombres) {
+            System.out.println("nombre del array Nombres ordenado = " + nombre);
+        }
+
     }
 }
