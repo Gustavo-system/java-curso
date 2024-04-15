0d30135720ef50bd6464b7736acf535e9bbc8bf4
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-04-15 16:55:14 -0600

    se reestructura curso de java agregan nuevos cursos de springboot

--- CursoPruebasUnitariasUdemy/src/main/java/com/example/cursopruebasunitariasudemy/calculadora/CalculadoraInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.cursopruebasunitariasudemy.calculadora;
-
-public interface CalculadoraInterface {
-    double sumar(double numero1, double numero2);
-    double restar(double numero1, double numero2);
-    double multiplicacion(double numero1, double numero2);
-    double divicion(double numero1, double numero2);
-}
