4a35f7ba95f5d295e89f79d0f0a2c2534c2a6ed2
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-05-07 17:57:51 -0600

    se agrega metodos del crud de mi primera api rest

---
+++ cursos/spring-boot-expert/modulo-07/app/src/main/java/com/cursos/app/controllers/UsuariosController.java
@@ -0,0 +1,57 @@
+package com.cursos.app.controllers;
+
+import com.cursos.app.models.User;
+import com.cursos.app.services.UserService;
+import jakarta.websocket.server.PathParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // Stereotipo que define que es una api rest
+@RequestMapping("/api/v1")
+public class UsuariosController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/up")
+    public String up(){
+        return "Hola mundo";
+    }
+
+    //@GetMapping("/users")
+    @RequestMapping(method = RequestMethod.GET, path = "/users") // una forma adicional para definir el recurso
+    public ResponseEntity<List<User>> get(){
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/users/filter")
+    public ResponseEntity<List<User>> filterUsers(@PathParam("inicial") String inicial){
+        return new ResponseEntity<>(userService.filterUser(inicial), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> postUser(@RequestBody User request){
+        return new ResponseEntity<>(userService.createUser(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Integer id, @RequestBody User request){
+        return new ResponseEntity<>(userService.updateUser(id, request), HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/users/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") Integer id){
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ cursos/spring-boot-expert/modulo-07/app/src/main/java/com/cursos/app/services/UserService.java
@@ -0,0 +1,85 @@
+package com.cursos.app.services;
+
+import com.cursos.app.models.User;
+import com.github.javafaker.Faker;
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private Faker faker;
+
+    /*
+     * Condigo para mi servicio
+     */
+    private List<User> users = new ArrayList<>();
+
+    @PostConstruct
+    public void init(){
+        for (int i = 0; i < 10; i++) {
+            users.add(new User(i + 1, faker.funnyName().name(), faker.name().username(), faker.dragonBall().character()));
+        }
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User getUserById(Integer id) {
+        return users.stream().filter(
+                user -> user.getId().equals(id)
+        ).findAny().orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("USER %s NOT FOUND", id))
+        );
+    }
+
+    public User createUser(User user) {
+        int lastIndex = users.size() - 1;
+        int lastId = users.get(lastIndex).getId();
+        int id = lastId + 1;
+        users.add(new User(id, user.getNickname(), user.getUsername(), user.getPassword()));
+        /*
+         * user.setId(id);
+         * users.add(user);
+         */
+        return users.get(id - 1);
+    }
+
+    public User updateUser(Integer id, User user) {
+        User userDataFind = getUserById(id);
+
+        userDataFind.setNickname(user.getNickname());
+        userDataFind.setUsername(user.getUsername());
+        userDataFind.setPassword(user.getPassword());
+
+        return userDataFind;
+    }
+
+    public void deleteUser(Integer id) {
+        User user = getUserById(id);
+        users.remove(user);
+        //users.removeIf(item -> item.getId().equals(id));
+    }
+
+    public List<User> filterUser(String inicial){
+        /*if(username == null || username.equals("")){
+            return users;
+        }*/
+        if (Strings.isEmpty(inicial)){
+            return users;
+        }
+
+        return users.stream().filter(item -> item.getUsername().startsWith(inicial)).toList();
+    }
+
+}
