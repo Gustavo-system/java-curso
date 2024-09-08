002cd2e89a07b9ab9c974effe8fafaac051cc6ec
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-09-08 11:55:48 -0600

    se agregan practicas

---
+++ practicas/spring-boot/ApiComplements/src/test/java/com/ggus/practicas/reforzandoconocimientos/ReforzandoconocimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggus.practicas.reforzandoconocimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReforzandoconocimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
