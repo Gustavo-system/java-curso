514f2bcee55704a912624d09d9e947971e50f458
GUSTAVO SOLAR GAONA <gustavo.solar@elektra.com.mx>
2024-09-12 17:24:45 -0600

    se agrega estrucutra para un nuevo proyecto de udemy - domina todos los modulos mas relevantes de springboot 2024

---
+++ spring-boot-cursos/domina-modulos-importantes-2024/DominaModulosRelevantes/src/main/java/com/curso/udemy/DominaModulosRelevantes/DominaModulosRelevantesApplication.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.DominaModulosRelevantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominaModulosRelevantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominaModulosRelevantesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-cursos/domina-modulos-importantes-2024/DominaModulosRelevantes/src/test/java/com/curso/udemy/DominaModulosRelevantes/DominaModulosRelevantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.udemy.DominaModulosRelevantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominaModulosRelevantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
